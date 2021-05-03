2a12c7b3d26089c4ed7410fdcd60da97fe2222f2
gendy-98 <ahmed.gendy98@eng-st.cu.edu.eg>
2021-05-03 11:55:25 +0200

    fifo perl added

---
+++ Model/Layer/CU_DP/Modules/FIFO_4_1.v
@@ -0,0 +1,119 @@
+`timescale 1ns / 1ps
+
+
+module 
+ FIFO_4_1 #(parameter
+///////////advanced parameters//////////
+	DATA_WIDTH 					= 32,
+	ADDRESS_BITS 				= 11,
+///////////architecture parameters//////
+	IFM_SIZE 					= 32,
+	IFM_DEPTH 					= 3,
+	KERNAL_SIZE 				= 2,
+	NUMBER_OF_FILTERS 			= 6,
+///////////generated parameters/////////
+	IFM_SIZE_NEXT           	= IFM_SIZE - KERNAL_SIZE + 1,
+	ADDRESS_SIZE_IFM        	= $clog2(IFM_SIZE*IFM_SIZE),
+	ADDRESS_SIZE_NEXT_IFM   	= $clog2(IFM_SIZE_NEXT*IFM_SIZE_NEXT),
+	ADDRESS_SIZE_WM         	= $clog2(IFM_DEPTH*NUMBER_OF_FILTERS),
+	NUMBER_OF_IFM           	= IFM_DEPTH,      
+	FIFO_SIZE               	= (KERNAL_SIZE-1)*IFM_SIZE + KERNAL_SIZE,
+	NUMBER_OF_IFM_NEXT      	= NUMBER_OF_FILTERS,
+	NUMBER_OF_WM            	= KERNAL_SIZE*KERNAL_SIZE,                              
+	NUMBER_OF_BITS_SEL_IFM_NEXT = $clog2(NUMBER_OF_IFM_NEXT)
+	)(
+	input clk,
+	input reset,
+	input fifo_enable,
+	input [DATA_WIDTH-1:0] fifo_data_in,
+	output [DATA_WIDTH-1:0] fifo_data_out_1,
+	output [DATA_WIDTH-1:0] fifo_data_out_2,
+	output [DATA_WIDTH-1:0] fifo_data_out_3,
+	output [DATA_WIDTH-1:0] fifo_data_out_4
+	);
+	reg	[DATA_WIDTH-1:0] FIFO  [FIFO_SIZE-1:0] ;
+	always @ (posedge clk or posedge reset)
+    begin
+        if(reset)
+		begin
+			FIFO[0] <= 0;
+			FIFO[1] <= 0;
+			FIFO[2] <= 0;
+			FIFO[3] <= 0;
+			FIFO[4] <= 0;
+			FIFO[5] <= 0;
+			FIFO[6] <= 0;
+			FIFO[7] <= 0;
+			FIFO[8] <= 0;
+			FIFO[9] <= 0;
+			FIFO[10] <= 0;
+			FIFO[11] <= 0;
+			FIFO[12] <= 0;
+			FIFO[13] <= 0;
+			FIFO[14] <= 0;
+			FIFO[15] <= 0;
+			FIFO[16] <= 0;
+			FIFO[17] <= 0;
+			FIFO[18] <= 0;
+			FIFO[19] <= 0;
+			FIFO[20] <= 0;
+			FIFO[21] <= 0;
+			FIFO[22] <= 0;
+			FIFO[23] <= 0;
+			FIFO[24] <= 0;
+			FIFO[25] <= 0;
+			FIFO[26] <= 0;
+			FIFO[27] <= 0;
+			FIFO[28] <= 0;
+			FIFO[29] <= 0;
+			FIFO[30] <= 0;
+			FIFO[31] <= 0;
+			FIFO[32] <= 0;
+			FIFO[33] <= 0;
+		end
+		else if(fifo_enable)
+		begin
+			FIFO[0] <= fifo_data_in;
+			FIFO[1] <= FIFO[0];
+			FIFO[2] <= FIFO[1];
+			FIFO[3] <= FIFO[2];
+			FIFO[4] <= FIFO[3];
+			FIFO[5] <= FIFO[4];
+			FIFO[6] <= FIFO[5];
+			FIFO[7] <= FIFO[6];
+			FIFO[8] <= FIFO[7];
+			FIFO[9] <= FIFO[8];
+			FIFO[10] <= FIFO[9];
+			FIFO[11] <= FIFO[10];
+			FIFO[12] <= FIFO[11];
+			FIFO[13] <= FIFO[12];
+			FIFO[14] <= FIFO[13];
+			FIFO[15] <= FIFO[14];
+			FIFO[16] <= FIFO[15];
+			FIFO[17] <= FIFO[16];
+			FIFO[18] <= FIFO[17];
+			FIFO[19] <= FIFO[18];
+			FIFO[20] <= FIFO[19];
+			FIFO[21] <= FIFO[20];
+			FIFO[22] <= FIFO[21];
+			FIFO[23] <= FIFO[22];
+			FIFO[24] <= FIFO[23];
+			FIFO[25] <= FIFO[24];
+			FIFO[26] <= FIFO[25];
+			FIFO[27] <= FIFO[26];
+			FIFO[28] <= FIFO[27];
+			FIFO[29] <= FIFO[28];
+			FIFO[30] <= FIFO[29];
+			FIFO[31] <= FIFO[30];
+			FIFO[32] <= FIFO[31];
+			FIFO[33] <= FIFO[32];
+		end
+	end
+
+	assign    fifo_data_out_1 = FIFO[(KERNAL_SIZE-1)*IFM_SIZE+(KERNAL_SIZE-1)];
+	assign    fifo_data_out_2 = FIFO[(KERNAL_SIZE-1)*IFM_SIZE+(KERNAL_SIZE-2)];
+	
+	assign    fifo_data_out_3 = FIFO[(KERNAL_SIZE-2)*IFM_SIZE+(KERNAL_SIZE-1)];
+	assign    fifo_data_out_4 = FIFO[(KERNAL_SIZE-2)*IFM_SIZE+(KERNAL_SIZE-2)];
+	
+endmodule
--- Model/Layer/CU_DP/Modules/fifo.pl
+++ Model/Layer/CU_DP/Modules/fifo.pl
@@ -10,13 +10,12 @@ use feature "switch";
 
 
 #argumets 
-#ARGV[0] outputs 
-#ARGV[1] stride 
-#ARGV[2] DATA_WIDTH 
-#ARGV[3] ADDRESS_BITS
-#ARGV[4] IFM_SIZE
+#ARGV[0] stride 
+#ARGV[1] DATA_WIDTH 
+#ARGV[2] ADDRESS_BITS
+#ARGV[3] IFM_SIZE
+#ARGV[4] IFM_DEPTH
 #ARGV[5] KERNAL_SIZE 
-#ARGV[6] IFM_DEPTH
 #ARGV[6] NUMBER_OF_FILTERS
 #
 
@@ -39,32 +38,26 @@ my $end_module = "endmodule";
 my $i_p = "input";
 my $o_p = "output";
 my $under_Score = "_";
+my $clog2 = "\$clog2";
 
 my $data_width = "DATA_WIDTH";
 my $address_bits = "ADDRESS_BITS";
 my $ifm_size = "IFM_SIZE";
-my $kernal_size = "KERNAL_SIZE";
 my $ifm_depth = "IFM_DEPTH";
+my $kernal_size = "KERNAL_SIZE";
 my $num_filters = "NUMBER_OF_FILTERS";
 
 #######################################################################################
 my $i = 0;
-
+my $j = 0;
 my $file_name;
 my $module_name;
 my $multi_name;
-if(lc ($ARGV[0]) eq "decimal"){
-	$module_name = "Dec_Divider";
-	$multi_name = "Dec_Multiplier";
-}
-if(lc ($ARGV[0]) eq "fixed"){
-	$module_name = "Fixed_Divider";
-	$multi_name = "Fixed_Multiplier";
-}
-if(lc ($ARGV[0]) eq "float"){
-	$module_name = "Float_Divider";
-	$multi_name = "Float_Multiplier";
-}
+my $num_outputs = $ARGV[5]*$ARGV[5];
+$module_name = "FIFO_$num_outputs$under_Score$ARGV[0]";
+
+
+
 
 
 $file_name = $module_name . ".v";
@@ -73,58 +66,91 @@ open my $fh, '>', $file_name
 
 print $fh <<"DONATE";
 $module $module_name $parameter
-	$data_width = $ARGV[1]
+///////////advanced parameters//////////
+	$data_width 					= $ARGV[1],
+	$address_bits 				= $ARGV[2],
+///////////architecture parameters//////
+	$ifm_size 					= $ARGV[3],
+	$ifm_depth 					= $ARGV[4],
+	$kernal_size 				= $ARGV[5],
+	$num_filters 			= $ARGV[6],
+///////////generated parameters/////////
+	IFM_SIZE_NEXT           	= IFM_SIZE - $kernal_size + 1,
+	ADDRESS_SIZE_IFM        	= $clog2(IFM_SIZE*IFM_SIZE),
+	ADDRESS_SIZE_NEXT_IFM   	= $clog2(IFM_SIZE_NEXT*IFM_SIZE_NEXT),
+	ADDRESS_SIZE_WM         	= $clog2(IFM_DEPTH*NUMBER_OF_FILTERS),
+	NUMBER_OF_IFM           	= IFM_DEPTH,      
+	FIFO_SIZE               	= ($kernal_size-1)*IFM_SIZE + $kernal_size,
+	NUMBER_OF_IFM_NEXT      	= NUMBER_OF_FILTERS,
+	NUMBER_OF_WM            	= $kernal_size*$kernal_size,                              
+	NUMBER_OF_BITS_SEL_IFM_NEXT = $clog2(NUMBER_OF_IFM_NEXT)
+	)(
+	$i_p clk,
+	$i_p reset,
+	$i_p fifo_enable,
+	$i_p [$data_width-1:0] fifo_data_in,
 DONATE
 
-if(lc ($ARGV[0]) eq "decimal"){
+if($ARGV[0] == 2){
+	print $fh "\t$i_p [$data_width-1:0] fifo_data_in_2,\n";
+}
+
+for($i = 1;$i < $num_outputs; $i = $i + 1){
 	print $fh <<"DONATE";
-	)(
-	$i_p [$data_width - 1:0] in1,
-	$i_p [$data_width - 1:0] in2,
-	$o_p [$data_width - 1:0] out
-	);
-	assign out = in1 + in2;
-$end_module
+	$o_p [$data_width-1:0] fifo_data_out_$i,
 DONATE
 }
-if(lc ($ARGV[0]) eq "fixed"){
-	print $fh <<"DONATE";
-	,E = $ARGV[3]
-	,M = $ARGV[2]
-	)(
-	$i_p [$data_width - 1:0] in1,
-	$i_p [$data_width - 1:0] in2,
-	$o_p [$data_width - 1:0] out
+
+print $fh <<"DONATE";
+	$o_p [$data_width-1:0] fifo_data_out_$i
 	);
-///////////////////////////////////////////////////////////////
-//////////////this code is not complete////////////////////////
-///////////////////////////////////////////////////////////////
-$end_module
+	reg	[$data_width-1:0] FIFO  [FIFO_SIZE-1:0] ;
+	always @ (posedge clk or posedge reset)
+    begin
+        if(reset)
+		begin
 DONATE
-}
-if(lc ($ARGV[0]) eq "float"){
-	print $fh <<"DONATE";
-	,E = $ARGV[3]
-	,M = $ARGV[2]
-	)(
-	$i_p [$data_width - 1:0] in1,
-	$i_p [$data_width - 1:0] in2,
-	$o_p [$data_width - 1:0] out
-	);
-	wire  [Data_Width-1:0] in2_rec;
-	 
-	assign  in2_rec =   (in2 == 32'b 01000000100000000000000000000000) ? (32'b 00111110100000000000000000000000) :
-						(in2 == 32'b 01000001000100000000000000000000) ? (32'b 00111101111000111000110110100100) :
-						(in2 == 32'b 01000001100000000000000000000000) ? (32'b 00111101100000000000000000000000) :
-						(in2 == 32'b 01000001110010000000000000000000) ? (32'b 00111101001000111101011100001010) :
-						(32'b 00111100111000111011110011010011);
+
+for($i = 0;$i < ($ARGV[5] - 1)*$ARGV[3] + $ARGV[5];$i = $i + 1){
+	print $fh "\t\t\tFIFO[$i] <= 0;\n";
 	
-	$multi_name	M1 ( .in1 (in1) ,.in2 (in2_rec),.out(out));
-	
-
-$end_module
-DONATE
 }
 
+print $fh <<"DONATE";
+		end
+		else if(fifo_enable)
+		begin
+DONATE
+if($ARGV[0] == 2){
+	print $fh "\t\t\tFIFO[0] <= fifo_data_in_2\n";
+	print $fh "\t\t\tFIFO[1] <= fifo_data_in\n";
+	for($i = 0;$i < (($ARGV[5] - 1)*$ARGV[3] + $ARGV[5])-3;$i = $i + 2){
+		print $fh "\t\t\tFIFO[${\($i+2)}] <= FIFO[$i];\n";
+		print $fh "\t\t\tFIFO[${\($i+3)}] <= FIFO[${\($i+1)}];\n";
+	}
+	if($ARGV[5] % 2 != 0){
+		print $fh "\t\t\tFIFO[${\($i+2)}] <= FIFO[$i];\n";
+	}
+}
+else{
+	print $fh "\t\t\tFIFO[0] <= fifo_data_in;\n";
+	for($i = 0;$i < (($ARGV[5] - 1)*$ARGV[3] + $ARGV[5])-1;$i = $i + 1){
+		print $fh "\t\t\tFIFO[${\($i+1)}] <= FIFO[$i];\n";
+	}
+}
+print $fh <<"DONATE";
+		end
+	end
+
+DONATE
+
+for($i = 1; $i <= $ARGV[5]; $i = $i + 1){
+	for($j = 1; $j <= $ARGV[5]; $j = $j + 1){
+		print $fh "\tassign    fifo_data_out_${\($j + ($i-1)*$ARGV[5])} = FIFO[($kernal_size-$i)*IFM_SIZE+($kernal_size-$j)];\n";
+	}
+	print $fh "\t\n";
+}
+
+print $fh "$end_module";
 
 close $fh or die "Couldn't Close File : $!";
